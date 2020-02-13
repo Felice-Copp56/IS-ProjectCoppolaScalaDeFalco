67e0005676ff731332734eb3cd7c9b9254f0b951
AndreaScala <AndreaScala@DESKTOP-TEQTE88>
2020-02-13 09:42:47 +0100

    Validazione cambi registrazione

--- WebContent/WEB-INF/jsp/homepageRegistrazione.jsp
+++ WebContent/WEB-INF/jsp/homepageRegistrazione.jsp
@@ -35,9 +35,7 @@ $(window).load(function () {
       
         <nav>
           <ul class="sf-menu">
-            <li class="current"><a href=".">Home</a></li>
-            <li><a href="our-story.html">Our Story</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li class="current"><a href=".">ELTANQUE <i class="fas fa-wine-glass-alt"></i> </a></li>
           </ul>
         </nav>
         <div class="clear"></div>
@@ -61,7 +59,7 @@ $(window).load(function () {
                 <li><span>Password: </span><input type=password name="textpass"></li>
                
               </ul>
-               <div class="alright"><button type="submit" class="btn">Login</button></div>
+               <div class="alright"><button type="submit" class="btn btn-danger">Login</button></div>
                
             </form>
             </div>
--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -20,52 +20,57 @@ import model.ClienteDAO;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html");  
-        PrintWriter out=response.getWriter();
-			try {
-				ClienteDAO dao = new ClienteDAO();
-				String user = request.getParameter("textuser");
-				String password = request.getParameter("textpass");
-				if(dao.doRetrieveByUsernamePassword(user, password)!=null)
-				{
-					ClienteBean bean = dao.doRetrieveByUsernamePassword(user, password);
-					System.out.println("Benvenuto "+bean.getNome()+" "+bean.getCognome());
-					RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/formFiltraTavoli.jsp");
-					request.getSession().setAttribute("user", bean.getUsername());
-					rd.forward(request, response);
-				}
-				else {
-					
-					RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/homepage.jsp");
-					rd.forward(request, response);
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	
-		
-		
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		RequestDispatcher rd = null;
+		String user = request.getParameter("textuser");
+		String password = request.getParameter("textpass");
+		try {
+			ClienteDAO dao = new ClienteDAO();
+			if (user.equals(" ") || password.equals(" ")) {
+				request.setAttribute("ERRORMSG", "Credenziali di accesso non valide");
+				System.out.println(request.getAttribute("ERRORMSG"));
+				rd = request.getRequestDispatcher("WEB-INF/jsp/homepage.jsp");
+			} else if (dao.doRetrieveByUsernamePassword(user, password) != null) {
+				ClienteBean bean = dao.doRetrieveByUsernamePassword(user, password);
+				rd = request.getRequestDispatcher("WEB-INF/jsp/formFiltraTavoli.jsp");
+				request.getSession().setAttribute("user", bean.getUsername());
+
+			} else {
+				request.setAttribute("ERRORMSG", "Credenziali di accesso non valide");
+				System.out.println(request.getAttribute("ERRORMSG"));
+				rd = request.getRequestDispatcher("WEB-INF/jsp/homepage.jsp");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
