9a0d2dc10644965aa140b2d7b8fbe7f7f981e73e
Felice <Felice@DESKTOP-K3R7HRS.homenet.telecomitalia.it>
2020-02-15 22:33:18 +0100

    Testing vari,manca ultima parte

--- src/testServlet/TestDatiAccesso.java
+++ src/testServlet/TestDatiAccesso.java
@@ -30,8 +30,216 @@ public class TestDatiAccesso extends Mockito {
 		servlet = new ModificaDatiAccesso();
 	}
 	@Test
-	public void ModificaDatiAccesso1()
+	public void ModificaDatiAccesso1() throws ServletException, IOException
 	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala123");
 		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso2() throws ServletException, IOException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea23andrea23andrea23andrea23");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala123");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso3() throws ServletException, IOException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","Andrea23");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala123");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso4() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		when(dao.existsUsername("marco23")).thenReturn(true);
+		servlet.setCliente(dao);
+		String errMessage = ("Username già presente");
+		request.setParameter("username","marco23");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala123");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso5() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea23");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "S");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	
+	@Test
+	public void ModificaDatiAccesso6() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea47");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "S");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso7() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea23");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala123Scala123Scala123");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso8() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea47");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala123Scala123Scala123");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso9() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea23");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso10() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea47");
+		request.setParameter("email", "Scala1.@gmail.com");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso11() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea23");
+		request.setParameter("email", "S");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso12() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea47");
+		request.setParameter("email", "S");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso13() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea23");
+		request.setParameter("email", "S@gmail@gmail.com");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso14() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		servlet.setCliente(dao);
+		String errMessage = ("Credenziali non valide");
+		request.setParameter("username","andrea47");
+		request.setParameter("email", "S@gmail@gmail.com");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
+	}
+	@Test
+	public void ModificaDatiAccesso15() throws ServletException, IOException, SQLException
+	{
+		ClienteDAO dao = mock(ClienteDAO.class);
+		when(dao.existsEmail("Felice1.@gmail.com")).thenReturn(true);
+		servlet.setCliente(dao);
+		String errMessage = ("Email già presente");
+		request.setParameter("username","andrea23");
+		request.setParameter("email", "Felice1.@gmail.com");
+		request.setParameter("password", "Scala");
+		
+		servlet.doGet(request,response);
+		String attribute = (String) request.getAttribute("ERRORMSG");
+		assertEquals(errMessage, attribute);
 	}
 }
