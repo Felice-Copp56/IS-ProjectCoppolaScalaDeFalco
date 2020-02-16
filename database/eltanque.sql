c452183c42285a9336e796140444e6fdf003e6a3
Felice <Felice@DESKTOP-K3R7HRS.homenet.telecomitalia.it>
2020-02-16 22:03:58 +0100

    Commit finale

--- database/eltanque.sql
+++ database/eltanque.sql
@@ -1,9 +1,7 @@
-
 create database eltanque;
 use eltanque;
 
 #drop database eltanque;
-
 create table cliente
 (
 	nome varchar(25) not null,
@@ -16,32 +14,46 @@ create table cliente
 	
     passwo varchar(25) not null
 );
+
+
+INSERT INTO `eltanque`.`cliente` (`nome`, `cognome`, `username`, `email`, `passwo`) VALUES ('Felice', 'Coppola', 'felice23', 'Felice1.@gmail.com', 'Felice123');
+
+
 create table gestore_tavoli
 (
 	codice_gestore_tavoli varchar(25) primary key not null
 );
+INSERT INTO `eltanque`.`gestore_tavoli` (`codice_gestore_tavoli`) VALUES ('GT-1');
+
 create table tavolo
 (
 	num_tavolo int primary key not null,
     
-    num_posti int not null,
-    
-    stato_tavolo bool
+    num_posti int not null
 );
+
+INSERT INTO `eltanque`.`tavolo` (`num_tavolo`, `num_posti`) VALUES ('1', '4');
+INSERT INTO `eltanque`.`tavolo` (`num_tavolo`, `num_posti`) VALUES ('2', '2');
+INSERT INTO `eltanque`.`tavolo` (`num_tavolo`, `num_posti`) VALUES ('3', '4');
+INSERT INTO `eltanque`.`tavolo` (`num_tavolo`, `num_posti`) VALUES ('4', '5');
+INSERT INTO `eltanque`.`tavolo` (`num_tavolo`, `num_posti`) VALUES ('5', '2');
+INSERT INTO `eltanque`.`tavolo` (`num_tavolo`, `num_posti`) VALUES ('6', '1');
 
 create table titolare
 (
 	codice_titolare varchar(25) primary key not null
 );
+
+INSERT INTO `eltanque`.`titolare` (`codice_titolare`) VALUES ('T-1');
+
 create table prenotazione
 (
     num_tavolo int not null,
-    foreign key(num_tavolo) references tavolo(num_tavolo),
+    foreign key(num_tavolo) references tavolo(num_tavolo) on delete cascade on update cascade,
     
     username varchar(25) not null,
-    foreign key(username) references cliente(username),
-    
-    
+    foreign key(username) references cliente(username) on update cascade,
+
     data date,
     
     ora_inizio time,
@@ -50,7 +62,8 @@ create table prenotazione
     
     primary key(num_tavolo,data,ora_fine,ora_inizio)
 );
-select p.num_tavolo,p.username,p.data,p.ora_inizio,p.ora_fine from prenotazione p where p.data between "2020-01-01" and "2020-03-01";
 
+INSERT INTO `eltanque`.`prenotazione` (`num_tavolo`, `username`, `data`, `ora_inizio`, `ora_fine`) VALUES ('1', 'felice23', '2020-01-01', '19:00:00', '20:00:00');
+INSERT INTO `eltanque`.`prenotazione` (`num_tavolo`, `username`, `data`, `ora_inizio`, `ora_fine`) VALUES ('1', 'felice23', '2020-01-01', '20:00:00', '21:00:00');
 
 
